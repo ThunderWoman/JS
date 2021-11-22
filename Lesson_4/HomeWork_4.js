60aa72c86def02cd50291c502ae61e983daeb460
ThunderWoman <yuliiasigva@gmail.com>
2021-11-22 14:52:56 +0200

    Практичні і домашні

--- Lesson_4/HomeWork_4.js
+++ Lesson_4/HomeWork_4.js
@@ -5,8 +5,8 @@ function rectangleSquare(a, b) {
     return rectangleS;
 }
 
-console.log(rectangleSquare(5, 7))
-document.write('Площа прямокутника: ' + rectangleSquare(5, 7) + '</br>')
+console.log(rectangleSquare(5, 7));
+document.write('Площа прямокутника: ' + rectangleSquare(5, 7) + '</br>');
 
 // - створити функцію яка обчислює та повертає площу кола з радіусом r
 
@@ -16,8 +16,8 @@ function circleSquare(r) {
     return circleS;
 }
 
-console.log(circleSquare(5))
-document.write('Площа круга: ' + circleSquare(5) + '</br>')
+console.log(circleSquare(5));
+document.write('Площа круга: ' + circleSquare(5) + '</br>');
 
 // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіусом r
 
@@ -26,8 +26,8 @@ function cylinderSquare(h, r) {
     return cylinderS;
 }
 
-console.log(cylinderSquare(5, 7))
-document.write('Площа циліндра: ' + cylinderSquare(5, 7) + '</br>')
+console.log(cylinderSquare(5, 7));
+document.write('Площа циліндра: ' + cylinderSquare(5, 7) + '</br>');
 
 // - створити функцію яка приймає масив та виводить кожен його елемент
 
@@ -35,62 +35,68 @@ let array = ['Hello', 'Okten', 35, true];
 
 function arrayItems(array) {
     for (let i = 0; i < array.length; i++) {
-        document.write(array[i])
+        document.write(array[i]);
     }
 }
 
-arrayItems(array)
+arrayItems(array);
 
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
 
 function textPar(text) {
-    document.write(`<p>${text}</p>`)
+    document.write(`<p>${text}</p>`);
 }
 
-textPar('Hello Mentor')
+textPar('Hello Mentor');
 
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
 
 function ulThree(textLi) {
-    document.write(`<ul>`)
-    document.write(`<li>${textLi}</li>`)
-    document.write(`<li>${textLi}</li>`)
-    document.write(`<li>${textLi}</li>`)
-    document.write(`</ul>`)
+    document.write(`<ul>`);
+    document.write(`<li>${textLi}</li>`);
+    document.write(`<li>${textLi}</li>`);
+    document.write(`<li>${textLi}</li>`);
+    document.write(`</ul>`);
 }
+
 ulThree('textLi');
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 
 function ulCycle(text, number) {
-    document.write(`<ul>`)
+    document.write(`<ul>`);
     for (let i = 0; i < number; i++) {
         document.write(`<li>${text}</li>`);
     }
-    document.write(`</ul>`)
+    document.write(`</ul>`);
 }
+
 ulCycle('textliCycle', 3);
 
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 
 let arrayPrimitive = ['Mentor', 35, true, 'Okten', 7, true];
-function primOl (arr){
-    document.write(`<ol>`)
-        for (let i = 0; i < arr.length; i++) {
-            document.write(`<li>${arr[i]}</li>`);
-        }
-    document.write(`</ol>`)
+
+function primOl(arr) {
+    document.write(`<ol>`);
+    for (let i = 0; i < arr.length; i++) {
+        document.write(`<li>${arr[i]}</li>`);
+    }
+    document.write(`</ol>`);
 }
+
 primOl(arrayPrimitive);
 
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
 
 let arrayObjects = [{id: 1, name: 'Julia', age: 35}, {id: 2, name: 'Natalia', age: 56}, {id: 3, name: 'Kerim', age: 4}, {id: 3, name: 'Emir', age: 2}];
+
 function objectAr(arr) {
     for (const elementAr of arr) {
         document.write(`<div>${elementAr.id}. ${elementAr.name} : ${elementAr.age}</div>`);
     }
 }
+
 objectAr(arrayObjects);

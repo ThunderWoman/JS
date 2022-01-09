810a643562bcc863848d3bb0d4ab1d96306c7d12
ThunderWoman <yuliiasigva@gmail.com>
2022-01-09 22:07:51 +0200

    Mini_Project

---
+++ MiniProject/MiniProject.js
@@ -0,0 +1,32 @@
+// В index.html
+// 1 получить массив объектов user с endpoint`а https://jsonplaceholder.typicode.com/users
+// 2 Вывести id,name всех user в index.html. Отдельный блок для каждого user.
+// 3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу user-details.html, которая имеет детальную информацию про объект на который кликнули
+
+// Стилизация проекта -
+// index.html - все блоки с user - по 2 в ряд. кнопки/ссылки находяться под информацией про user.
+
+let userDiv = document.createElement('div');
+userDiv.classList.add('userWrap');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then((value) => value.json())
+    .then(users => {
+        for (let userItem of users) {
+            let user = document.createElement('div');
+            user.classList.add('user');
+            user.innerHTML = `<h3>${userItem.id}. ${userItem.name}</h3>`;
+
+            let button = document.createElement('button');
+            button.innerText = `More details`;
+            button.className = "button";
+            button.onclick = () => {
+                location.href = `user-details.html`;
+                localStorage.setItem('userDetails', JSON.stringify(user.id));
+            }
+            user.append(button);
+            userDiv.appendChild(user);
+        }
+        document.body.append(userDiv);
+    });
+
---
+++ MiniProject/post-details.js
@@ -0,0 +1,48 @@
+// На странице post-details.html:
+// 7 Вывести всю, без исключения, информацию про объект post на кнопку/ссылку которого был совершен клик ранее.
+// 8 Ниже информации про пост, вывести все комментарии текущего поста (эндпоинт для получения информации - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
+
+// Стилизация проекта -
+
+// post-details.html - блок с информацией про пост вверху. Комментарии - по 4 в ряд.
+// Все без исключения элементы, который характеризируют user,post,comment  визуализировать, так, что бы было видно их блоки (дать задний фон + margin. Иными словами - крайне четкая сетка)
+
+
+let postId = JSON.parse(localStorage.getItem('postId'));
+let divBlock = document.createElement('div');
+document.body.appendChild(divBlock);
+divBlock.classList.add('wrap');
+let getPost =
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+        .then(value => value.json())
+        .then(post => {
+            let postBlock = document.createElement('div');
+            postBlock.classList.add('block');
+            for (const postUser in post) {
+                let p = document.createElement('p');
+                p.classList.add('border')
+                p.innerText = `${postUser}: ${post[postUser]}`;
+                postBlock.appendChild(p)
+            }
+            getCommentsByPost()
+            divBlock.appendChild(postBlock)
+        });
+let getCommentsByPost = () => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+        .then(value => value.json())
+        .then(comments => {
+            let commentsUser = document.createElement('div');
+            commentsUser.classList.add('wrap');
+            for (const comment of comments) {
+                let commentUser = document.createElement('div');
+                commentUser.classList.add('comment-block')
+                for (const commentKey in comment) {
+                    let p = document.createElement('p');
+                    p.innerText = `${commentKey}: ${comment[commentKey]}`;
+                    commentUser.appendChild(p);
+                }
+                commentsUser.appendChild(commentUser);
+            }
+            divBlock.appendChild(commentsUser)
+        })
+}
